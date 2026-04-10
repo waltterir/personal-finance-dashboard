3e18b11ab010d32a0e5697c49987918f4f82f4cf
waltterir <riihimakiwaltteri@gmail.com>
2026-04-10 19:44:44 +0300

    feat: Add create transaction logic and router endpoint

--- backend/crud/transaction_crud.py
+++ backend/crud/transaction_crud.py
@@ -1,7 +1,21 @@
 from sqlmodel import Session, select
-from models.models import Transaction
+from models.models import Transaction, TransactionCreate
 from fastapi import HTTPException, status
+from datetime import datetime
 
 def get_transactions(session: Session):
     statement = select(Transaction)
     return session.exec(statement).all()
+
+def create_transaction(session: Session, transaction_in: TransactionCreate):
+    transaction = Transaction(
+        description=transaction_in.description, 
+        amount=transaction_in.amount, 
+        category=transaction_in.category, 
+        transactions_date=transaction_in.transactions_date,
+        created_at=datetime.now()
+        )
+    session.add(transaction)
+    session.commit()
+    session.refresh(transaction)
+    return transaction
--- backend/routes/transaction.py
+++ backend/routes/transaction.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends, status
-from models.models import TransactionRead
+from models.models import TransactionRead, TransactionCreate
 from crud import transaction_crud as crud
 from sqlmodel import Session
 from database.database import get_session
@@ -9,3 +9,7 @@ router = APIRouter(prefix="/transactions", tags=["transactions"])
 @router.get("/", response_model=list[TransactionRead])
 def get_transactions(*, session: Session = Depends(get_session)):
     return crud.get_transactions(session)
+
+@router.post("/", response_model=TransactionRead, status_code=status.HTTP_201_CREATED)
+def create_transaction(*, session: Session = Depends(get_session), transaction_in: TransactionCreate):
+    return crud.create_transaction(session, transaction_in)
